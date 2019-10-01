0efba822cee8c0def5beed8a873cadaf34bbb32a
Randa Khairy <randakhairy_95@hotmail.com>
2019-10-01 12:52:51 +0200

    intial header

---
+++ ultrasonic.h
@@ -0,0 +1,15 @@
+/*
+ * ultrasonic.h
+ *
+ *  Created on: Oct 1, 2019
+ *      Author: Randa
+ */
+
+#ifndef ULTRASONIC_H_
+#define ULTRASONIC_H_
+
+#include "std_types.h"
+
+uint8 ULTRA_read();
+
+#endif /* ULTRASONIC_H_ */
