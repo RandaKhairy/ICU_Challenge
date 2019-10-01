91446ae2b5404ea75dc546309a32dca67e6cf099
OmarSha3rawy <omar.aadel58@gmail>
2019-10-01 11:21:35 +0200

    counting objects with ultrasonic stub

--- main.c
+++ main.c
@@ -6,13 +6,25 @@
  */ 
 
 #include <avr/io.h>
+#include "std_types.h"
+//#include "randa"
+uint8 ULTRA_read();
 
 
 int main(void)
 {
+	uint8 counts = 0;
     /* Replace with your application code */
     while (1) 
     {
-    }
+		if(ULTRA_read())
+		{
+			counts++;
+		}
+	}
 }
 
+uint8 ULTRA_read()
+{
+	return (uint8)0 ;
+}
