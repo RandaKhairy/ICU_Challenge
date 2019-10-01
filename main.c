a5f2d236c8558d468bc98fb7fded9c668f2fae23
OmarSha3rawy <omar.aadel58@gmail>
2019-10-01 16:13:15 +0200

    after callbacks

--- main.c
+++ main.c
@@ -2,29 +2,40 @@
  * ICU_development.c
  *
  * Created: 10/1/2019 9:51:26 AM
- * Author : omara
+ * Author : omar sha3rawy
  */ 
 
 #include <avr/io.h>
+#include <util/delay.h>
 #include "std_types.h"
 //#include "randa"
-uint8 ULTRA_read();
 
+uint16 counts = 0;
 
-int main(void)
+void ULTRA_read(void (*ptr)(uint16));
+
+void increment_function(uint16 dis)
 {
-	uint8 counts = 0;
-    /* Replace with your application code */
-    while (1) 
-    {
-		if(ULTRA_read())
-		{
-			counts++;
-		}
+	if (dis < 400)
+	{
+	counts++;		
 	}
 }
 
-uint8 ULTRA_read()
+//void (*ptr)(uint16) = increment_function;
+
+int main(void)
+{
+    while (1) 
+    {
+		ULTRA_read(increment_function);
+		_delay_ms(25);
+	}
+}
+
+/*
+uint8 ULTRA_read(ptr)
 {
 	return (uint8)0 ;
 }
+*/
