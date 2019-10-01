371dcb012a2a7b6389ffe4300b053947682af281
OmarSha3rawy <omar.aadel58@gmail>
2019-10-01 10:59:21 +0200

    initial empty main

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * ICU_development.c
+ *
+ * Created: 10/1/2019 9:51:26 AM
+ * Author : omara
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
