268f9ce9f45da62744011bdfd9c26b3941d796ac
Mohammed Nassar <mohammedalaanassar@hotmail.com>
2019-10-02 18:19:04 +0200

    ICU Software Module  --- > unit testing succeed Source file Header file

--- ICU.c
+++ ICU.c
@@ -1,60 +1,93 @@
 /*
  * ICU.c
  *
- *  Created on: 1 Oct 2019
- *      Author: Mariam Taha
+ *  Created on: Oct 2, 2019
+ *      Author: Mohammed
  */
-
 #include "ICU.h"
 
-/* Global variables to hold the address of the call back function in the application */
-static volatile void (*g_callBackPtr)(void) = NULL;
+static uint8 EDGE=0;
+static volatile uint8 EXINT=0;
 
-
-void Icu_init(void)
+/* External INT2 Interrupt Service Routine */
+ISR(INT2_vect)
 {
-	Timer1_overflow();
-	External_interrupt_init();
-}
 
-void Icu_clearTimerValue(void)
-{
-	TCNT1 = 0;
-}
-
-uint16 Icu_getInputCaptureValue(void)
-{
-	return TCNT1;
-}
-
-
-void Icu_setEdgeDetectionType(const uint8 a_edgeType)
-{
-	if(a_edgeType==FALLING)
-	{
-		MCUCSR &=~(1<<ISC2);
+	if (EDGE==0){
+		//DIO_write(testled1,HIGH);
+		EXINT=1;
+		//DIO_write(testled1,HIGH);
 	}
-	else if(a_edgeType==RISING)
+	else if (EDGE==1)
 	{
-		MCUCSR |=(1<<ISC2);
+		EXINT=1;
+	//	DIO_write(testled2,HIGH);
 	}
 }
 
 
-void Icu_DeInit(void)
+static void Timer2_init_normal_mode(void)
 {
-	GICR &=~(1<<INT2);
+	TCNT2 = 0; //timer initial value
+	/* Configure the timer control register
+	 * 1. Non PWM mode FOC2=1
+	 * 2. Normal Mode WGM21=0 & WGM20=0
+	 * 3. Normal Mode COM20=0 & COM21=0
+	 * 4. no prescaler clock = F_CPU/1024 CS20=1 CS21=1 CS22=1
+	 */
+	TCCR2 = (1<<FOC2)  | (1<<CS20)| (1<<CS22) | (1<<CS21) ; //
 }
 
-void Icu_setCallBack(void(*a_ptr)(void))
+void INT2_Init_Raising_FirstTime(void)
 {
-	/* Save the address of the Call back function in a global variable */
-	g_callBackPtr = a_ptr;
-	Interrupt_setCallBack(Call_app);
+
+	SREG   &= ~(1<<7);      // Disable interrupts by clearing I-bit
+	DDRB   &= (~(1<<PB2));   // Configure INT2/PB2 as input pin
+	GICR   |= (1<<INT2);	// Enable external interrupt pin INT2
+	MCUCSR &= ~(1<<ISC2);    // Trigger INT2 with the raising edge
+	SREG   |= (1<<7);       // Enable interrupts by setting I-bit
 }
 
-void Call_app(void)
+static void INT2_Init_Raising(void)
 {
-	(*g_callBackPtr)();
+//	SREG   &= ~(1<<7);      // Disable interrupts by clearing I-bit
+	MCUCSR &= ~(1<<ISC2);    // Trigger INT2 with the raising edge
+	//SREG   |= (1<<7);       // Enable interrupts by setting I-bit
 }
 
+static void INT2_Init_Falling(void)
+{
+	//SREG   &= ~(1<<7);      // Disable interrupts by clearing I-bit
+	MCUCSR |= (1<<ISC2);    // Trigger INT2 with the falling edge
+//	SREG   |= (1<<7);       // Enable interrupts by setting I-bit
+}
+
+
+
+
+void ICU(void){
+
+	//uint8 TIME=0;
+
+	if ( (EXINT==1) && (EDGE == 0) )
+	{
+	INT2_Init_Falling();
+	Timer2_init_normal_mode();
+	EDGE = 1 ;
+	EXINT = 0;
+	}
+	else if ( (EXINT==1) && (EDGE == 1) )
+	{
+		uint8 TIME=TCNT2;
+		char arr[50];
+		//PORTC = TCNT2;
+		//LCD_displayString("TCNT= ");
+		sprintf(arr,"%d",TIME);
+		//LCD_displayString(arr);
+	//TIME = TCNT2;
+	INT2_Init_Raising();
+	EDGE = 0 ;
+	EXINT = 0;
+	}
+
+}
--- ICU.h
+++ ICU.h
@@ -1,27 +1,21 @@
 /*
  * ICU.h
  *
- *  Created on: 1 Oct 2019
- *      Author: Mariam Taha
+ *  Created on: Oct 2, 2019
+ *      Author: Mohammed
  */
 
 #ifndef ICU_H_
 #define ICU_H_
 
-#include "Timer.h"
-#include "Ex_interrupt.h"
-#include "std_types.h"
+#include <avr/io.h>		/* Include AVR std. library file */
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "DIO.h"
+//#include "lcd.h"
+#include <stdio.h>
 
-#define FALLING 0
-#define RISING 1
-
-void Icu_init(void);
-void Icu_clearTimerValue(void);
-uint16 Icu_getInputCaptureValue(void);
-void Icu_setEdgeDetectionType(const uint8 a_edgeType);
-void Icu_DeInit(void);
-void Icu_setCallBack(void(*a_ptr)(void));
-void Call_app(void);
-
+extern void ICU(void);
+extern void INT2_Init_Raising_FirstTime(void);
 
 #endif /* ICU_H_ */
