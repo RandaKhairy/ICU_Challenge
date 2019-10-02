0b6cbc3b85f5e4fad07e85b252e5c531c4bd4958
Mohammed Nassar <mohammedalaanassar@hotmail.com>
2019-10-02 12:34:53 +0200

    PWM Module Source file Header file Phase_Correct_PWM(double duty_cycle) Function

--- PWM.h
+++ PWM.h
@@ -1,26 +1,18 @@
 /*
- * DIO.h
+ * PWM.h
  *
- * Created: 9/20/2019 3:42:23 PM
- *  Author: omar sha3rawy
- */ 
+ *  Created on: Oct 2, 2019
+ *      Author: Mohammed
+ */
+
+#ifndef PWM_H_
+#define PWM_H_
 
 
-#ifndef DIO_H_
-#define DIO_H_
-
-#include <avr/io.h> // rewrite it
-#include "DIO_configuration.h"
-#include "std_types.h"
-
-#define MAX_NUM_OF_PINS	(uint8)32
-
-typedef enum{NOK,OK}func_state;
+#include <avr/io.h>		/* Include AVR std. library file */
+#include <util/delay.h>
 
 
-func_state DIO_init();
-func_state DIO_read(pintype device, uint8* result);
-//func_state DIO_read (uint8 port,uint8 pin,uint8 * Pval);
-func_state DIO_write(pintype device, uint8 value);
+extern void Phase_Correct_PWM(double duty_cycle);
 
-#endif /* DIO_H_ */
+#endif /* PWM_H_ */
